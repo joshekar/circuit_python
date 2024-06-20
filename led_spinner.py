aedf7c3173632a9913756c97b209328f25f98d25
Jyothi Shekar <65193897+joshekar@users.noreply.github.com>
2024-06-20 13:50:14 -0700

    Create led_spinner.py

---
+++ led_spinner.py
@@ -0,0 +1,19 @@
+import board
+from digitalio import DigitalInOut, Direction, Pull
+import time
+import neopixel
+
+led = neopixel.NeoPixel(board.NEOPIXEL, 10)
+
+switch = DigitalInOut(board.SLIDE_SWITCH)
+switch.direction = Direction.INPUT
+switch.pull = Pull.UP
+
+print("Switch value is :", switch.value)
+
+while True:
+    for i in range(10) :   
+        led[i] = (255,255,0)
+        time.sleep(0.1)
+        led[i] = (0,0,0)
+        
