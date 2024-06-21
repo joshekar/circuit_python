3214b437ef14e2b3451625dbb377e68277d4a892
Jyothi Shekar <65193897+joshekar@users.noreply.github.com>
2024-06-21 11:53:25 -0700

    Create led_butterfly_flapper.py

---
+++ led_butterfly_flapper.py
@@ -0,0 +1,28 @@
+import board
+from digitalio import DigitalInOut, Direction, Pull
+import time
+import neopixel
+
+led = neopixel.NeoPixel(board.NEOPIXEL, 10)
+
+switch = DigitalInOut(board.SLIDE_SWITCH)
+switch.direction = Direction.INPUT
+switch.pull = Pull.UP
+
+print("Switch value is :", switch.value)
+
+while True:
+    if (switch.value==True):
+        for i in range(5) :   
+            led[i] = (255,0,0)
+            led[9-i] = (255,0,0)
+            time.sleep(0.5)
+            led[i] = (0,0,0)
+            led[9-i] = (0,0,0)
+
+        for i in range(3,0,-1) :   
+            led[i] = (0,0,255)
+            led[9-i] = (0,0,255)
+            time.sleep(0.5)
+            led[i] = (0,0,0)
+            led[9-i] = (0,0,0)
