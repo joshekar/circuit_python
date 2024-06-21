2091d796263961ec9c4d4409ab7c1d0b9061f75c
Jyothi Shekar <65193897+joshekar@users.noreply.github.com>
2024-06-21 10:21:26 -0700

    Update neopixel_led.py
    Added if condition for switch value

--- neopixel_led.py
+++ neopixel_led.py
@@ -3,7 +3,7 @@ from digitalio import DigitalInOut, Direction, Pull
 import time
 import neopixel
 
-led = neopixel.NeoPixel(board.NEOPIXEL, 1)
+led = neopixel.NeoPixel(board.NEOPIXEL, 10)
 
 switch = DigitalInOut(board.SLIDE_SWITCH)
 switch.direction = Direction.INPUT
@@ -13,8 +13,17 @@ print("Switch value is :", switch.value)
 
 while True:
     if (switch.value==True):
-        led[0] = (255,0,0)
-        
-    else:
-        led[0] = (0,255,0)
+        for i in range(10) :   
+            led[i] = (255,0,0)
+            time.sleep(0.1)
+            led[i] = (0,0,0)
     
+        for i in range(10) :   
+            led[i] = (0,255,0)
+            time.sleep(0.1)
+            led[i] = (0,0,0)
+    
+        for i in range(10) :   
+            led[i] = (0,0,255)
+            time.sleep(0.1)
+            led[i] = (0,0,0)
