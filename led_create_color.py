6810fcf84b3334501e3924ee0f4ca2c74f628f3f
Jyothi Shekar <65193897+joshekar@users.noreply.github.com>
2024-06-21 10:56:50 -0700

    Create led_create_color.py

---
+++ led_create_color.py
@@ -0,0 +1,29 @@
+import board
+from digitalio import DigitalInOut, Direction, Pull
+import time
+import neopixel
+
+led = neopixel.NeoPixel(board.NEOPIXEL, 10)
+
+switch = DigitalInOut(board.SLIDE_SWITCH)
+switch.direction = Direction.INPUT
+switch.pull = Pull.UP
+
+print("Switch value is :", switch.value)
+
+while True:
+    if (switch.value==True):
+        for i in range(10) :   
+            led[i] = (255,111,0)
+            time.sleep(0.1)
+            led[i] = (0,0,0)
+    
+        for i in range(10) :   
+            led[i] = (0,255,33)
+            time.sleep(0.1)
+            led[i] = (0,0,0)
+    
+        for i in range(10) :   
+            led[i] = (55,0,255)
+            time.sleep(0.1)
+            led[i] = (0,0,0)
