5089d15f75b5be2e7f4678dc9f631ce8c7461cca
Jyothi Shekar <65193897+joshekar@users.noreply.github.com>
2024-06-26 11:38:03 -0700

    Create cp_button_pass.py

---
+++ cp_button_pass.py
@@ -0,0 +1,12 @@
+import time
+from adafruit_circuitplayground import cp
+
+while True:
+    if cp.button_a:
+        while (cp.button_a):
+            pass
+        print("pressed a")
+    if cp.button_b:
+        while (cp.button_b):
+            pass
+        print("pressed b")
