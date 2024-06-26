fb017601f4836c4dcd0ec9e42a989c9001dfe97d
Jyothi Shekar <65193897+joshekar@users.noreply.github.com>
2024-06-26 09:50:14 -0700

    Create cp_pixel_led.py

---
+++ cp_pixel_led.py
@@ -0,0 +1,10 @@
+from adafruit_circuitplayground import cp
+
+
+while True:
+    cp.pixels[0] =(255,0,0)
+    cp.pixels[2] =(255,255,0)
+    cp.pixels[4] =(0,255,0)
+    cp.pixels[5] =(0,255,255)
+    cp.pixels[7] =(0,0,255)
+    cp.pixels[9] =(255,0,255)
